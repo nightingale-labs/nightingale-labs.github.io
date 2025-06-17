6ba47a7dab89eb59c6f618f7d131bb8c9b1d18b6
kushinm <kushinm11@gmail.com>
2025-06-17 04:19:58 -0700

    page updates

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -10,28 +10,27 @@ interface Person {
   website?: string;
 }
 
-
 const People: React.FC = () => {
   return (
     <div
       style={{
         fontFamily: "'Roboto Mono', monospace",
       }}
-      className="mx-auto flex flex-col space-y-6"
+      className="mx-auto flex flex-col space-y-8"
     >
-      <div className="">
-        <h2 className="text-4xl mb-2">research team</h2>
-        <div className="border-2 border-black bg-white rounded-lg mb-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div>
+        <h2 className="text-5xl mb-4">research team</h2>
+        <div className="border-2 border-black bg-white rounded-lg mb-12 grid grid-cols-1 md:grid-cols-3 gap-8 p-4">
           {researchTeam.map((person, idx) => (
-            <PersonPortrait person={{...person, id: idx + 200}} />
+            <PersonPortrait person={{ ...person, id: idx + 200 }} key={idx + 200} />
           ))}
         </div>
       </div>
-      <div className="">
-        <h2 className="text-4xl mb-2">advisory board</h2>
-        <div className="border-2 border-black bg-white rounded-lg grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div>
+        <h2 className="text-5xl mb-4">advisory board</h2>
+        <div className="border-2 border-black bg-white rounded-lg grid grid-cols-1 md:grid-cols-4 gap-8 p-4">
           {advisoryBoard.map((person, idx) => (
-            <PersonPortrait person={{...person, id: idx + 100}} />
+            <PersonPortrait person={{ ...person, id: idx + 100 }} key={idx + 100} />
           ))}
         </div>
       </div>
@@ -45,17 +44,17 @@ const PersonPortrait: React.FC<{ person: Person }> = ({ person }) => {
       id="people-portrait"
       target="_blank"
       href={person.website}
-      key={person.id}
-      className="px-2 py-4 text-sm flex flex-col items-center cursor-pointer hover:underline rounded-lg group"
+      className="px-4 py-6 text-base flex flex-col items-center cursor-pointer hover:underline rounded-lg group"
+      rel="noopener noreferrer"
     >
       <img
         id="people-portrait"
         src={person.imageUrl}
         alt={person.name}
-        className="w-32 h-32 rounded-full object-cover cursor-pointer group-hover:outline group-hover:outline-4 group-hover:outline-[#ffcc04] "
+        className="w-40 h-40 rounded-full object-cover cursor-pointer group-hover:outline group-hover:outline-4 group-hover:outline-[#ffcc04]"
       />
-      <p className="mt-4 text-lg font-semibold">{person.name}</p>
-      <p className="text-md">{person.affiliation}</p>
+      <p className="mt-6 text-2xl font-semibold">{person.name}</p>
+      <p className="text-lg">{person.affiliation}</p>
     </a>
   );
 };
--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -4,27 +4,28 @@ const Research: React.FC<{}> = () => {
   return (
     <div
       style={{
-        fontFamily: "'Roboto Mono', monospace",
+        fontFamily: "'Inter', 'Helvetica Neue', Arial, sans-serif",
       }}
       className="mx-auto flex flex-col space-y-6 max-w-10xl"
     >
       <div>
-        <h2 className="text-4xl mb-2">research</h2>
-        <div className="outline rounded p-4 bg-white">
+        <h2 className="text-5xl mb-4">research</h2>
+        <div className="border-2 border-black rounded p-4 bg-white">
           <div className="flex flex-col w-full space-y-8">
             {researchPreviews.map((o) => (
               <div
                 key={o.imgSrc}
-                className="bg-gray-100 rounded-lg p-4 border border-gray-200 mb-4"
+                className="bg-gray-100 rounded-lg px-10 py-4 border border-gray-200 mb-4"
               >
-                <span className="font-bold text-2xl mb-2 block">{o.title}</span>
+                <span className="font-bold text-3xl mb-2 block">{o.title}</span>
+                <span className="italic text-2xl mb-2 block">{o.tagline}</span>
                 <img
-                  style={{ maxHeight: "16em", float: "left", marginRight: "1.5rem", marginBottom: "1rem",marginTop: "1rem"}}
+                  style={{ maxHeight: "16em", float: "left", marginRight: "1.5rem", marginBottom: "1rem", marginTop: "1rem" }}
                   className="rounded-sm"
                   src={o.imgSrc}
                   alt={o.imgAlt}
                 />
-                <p className="text-base text-gray-800 text-left">{o.abstract}</p>
+                <p className="text-lg text-gray-800 text-left">{o.abstract}</p>
                 <div style={{ clear: "both" }} />
               </div>
             ))}
